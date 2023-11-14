4620bd71367265a6c4494b55309628a0ffe6ae0f
mattbencomo <Matthew.Bencomo@gmail.com>
2023-11-13 22:12:12 -0800

    added react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,98 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='projects' element={<Projects />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <Navbar expand='lg' className='bg-body-tertiary'>
+        <Container>
+          <Navbar.Brand>Matt Bencomo</Navbar.Brand>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link>
+                <Link className='nav-link' to='/'>
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link className='nav-link' to='/projects'>
+                  Projects
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+}
+
+function Projects() {
+  return (
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
